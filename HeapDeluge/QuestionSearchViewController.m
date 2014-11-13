e37b740c537e5c3e5a97dc6700ad05b243fbffcb
Will Richman <wrichman@gmail.com>
2014-11-13 09:23:04 -0800

    Fixed question search vote colors.

--- HeapDeluge/QuestionSearchViewController.m
+++ HeapDeluge/QuestionSearchViewController.m
@@ -38,10 +38,14 @@
     cell.answerCount.text = [NSString stringWithFormat:@"%li", (long)questionToDisplay.answers];
     cell.voteCount.text = [NSString stringWithFormat:@"%li", (long)questionToDisplay.score];
     if (questionToDisplay.answers > 0) {
-        cell.answerView.backgroundColor = [UIColor colorWithRed:0 green:0.8 blue:0 alpha:0.8];
+        cell.answerView.backgroundColor = [UIColor colorWithRed:0 green:0.8 blue:0 alpha:0.6];
+    } else {
+        cell.answerView.backgroundColor = [UIColor colorWithRed:1 green:0 blue:0 alpha:0.65];
     }
     if (questionToDisplay.score > 0) {
-        cell.voteView.backgroundColor = [UIColor colorWithRed:0 green:0.8 blue:0 alpha:0.8];
+        cell.voteView.backgroundColor = [UIColor colorWithRed:0 green:0.8 blue:0 alpha:0.6];
+    } else {
+        cell.voteView.backgroundColor = [UIColor colorWithRed:1 green:0 blue:0 alpha:0.65];
     }
     cell.username.text = questionToDisplay.ownerName;
     NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
