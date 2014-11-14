cd1d6836fd83bc06a4234f407ca8b048c6dc0b91
Will Richman <wrichman@gmail.com>
2014-11-14 11:01:20 -0800

    Recreated Menu VC.

---
+++ HeapDeluge/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  HeapDeluge
+//
+//  Created by William Richman on 11/14/14.
+//  Copyright (c) 2014 Will Richman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@end
